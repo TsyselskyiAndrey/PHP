a180e97970c53037e3f0d76e0eb792da870b00a7
TsyselskyiAndrey <tsyselskyiandrey@gmail.com>
2024-10-11 13:38:19 +0200

    update

--- Laravel_AJAX/routes/api.php
+++ Laravel_AJAX/routes/api.php
@@ -1,39 +1,36 @@
-class ApiController extends Controller
-{
-    public function index()
-    {
-        $posts = Post::with('comments')->get();
-        return response()->json($posts);
-    }
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CommentController;
 
-    public function store(Request $request)
-    {
-        $post = Post::create([
-            'title' => $request->input('title'),
-            'content' => $request->input('content')
-        ]);
-        return response()->json($post);
-    }
+// Маршрут для получения списка всех постов
+Route::get('/posts', [ApiController::class, 'index']);
+// ApiController::class - указывает на контроллер ApiController
+// 'index' - метод контроллера, который будет вызываться при запросе
 
-    public function show($id)
-    {
-        $post = Post::with('comments')->find($id);
-        return response()->json($post);
-    }
+// Маршрут для создания нового поста
+Route::post('/posts', [ApiController::class, 'store']);
 
-    public function update(Request $request, $id)
-    {
-        $post = Post::find($id);
-        $post->update([
-            'title' => $request->input('title'),
-            'content' => $request->input('content')
-        ]);
-        return response()->json($post);
-    }
+// Маршрут для получения информации о конкретном посте
+Route::get('/posts/{id}', [ApiController::class, 'show']);
+// {id} - параметр в URL, который передается в метод show
 
-    public function destroy($id)
-    {
-        Post::destroy($id);
-        return response()->json(['message' => 'Пост удален']);
-    }
-}
+// Маршрут для обновления информации о посте
+Route::put('/posts/{id}', [ApiController::class, 'update']);
+
+// Маршрут для удаления поста
+Route::delete('/posts/{id}', [ApiController::class, 'destroy']);
+
+// Маршрут для создания комментария к посту
+Route::post('/posts/{id}/comments', [CommentController::class, 'store']);
+
+// Маршрут для получения комментариев к конкретному посту
+Route::get('/posts/{id}/comments', [CommentController::class, 'index']);
+
+// Маршрут для обновления информации о комментарии
+Route::put('/comments/{commentId}', [CommentController::class, 'update']);
+
+// Маршрут для удаления комментария
+Route::delete('/comments/{commentId}', [CommentController::class, 'destroy']);
+Route::post('/login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
